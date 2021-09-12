012a4a4e722c65583d7233b56de34679088f3048
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-12 16:22:57 +0300

    Stick menu withoud callbacks

--- src/js/modules/menuSticky.js
+++ src/js/modules/menuSticky.js
@@ -1,17 +1,14 @@
-const stickMenu = (menuElem, selectorSectionToSticky, ...cb) => {
-    [cbAfter, cbBefore] = cb;
+const stickMenu = (menuElem, selectorSectionToSticky) => {
     window.addEventListener("scroll", () => {
         const targetMenu = document.querySelector(menuElem);
         const targetSection = document.querySelector(selectorSectionToSticky);
         if(window.scrollY >= targetMenu.offsetTop){
             targetMenu.classList.remove("header-bottom");
             targetMenu.classList.add("header-top");
-            try{cbAfter()} catch {null}
         }
         if(targetSection.offsetHeight-targetMenu.offsetHeight>=window.scrollY){
             targetMenu.classList.add("header-bottom");
             targetMenu.classList.remove("header-top");
-            try{cbBefore()} catch {null}
         }
     });
 }
